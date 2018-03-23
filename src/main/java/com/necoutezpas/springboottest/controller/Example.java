ab69dc70b3d5cbc614e1a1d92539c5959ba6a5f2
Pascal Le Levier <plelevier@accengage.com>
2018-03-23 11:49:55 +0100

    Add json file for testing posts and add a route for testing simple gets

--- src/main/java/com/necoutezpas/springboottest/controller/Example.java
+++ src/main/java/com/necoutezpas/springboottest/controller/Example.java
@@ -1,6 +1,7 @@
 package com.necoutezpas.springboottest.controller;
 
 import com.necoutezpas.springboottest.model.Test;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,13 @@ import reactor.core.publisher.Mono;
 
 @RestController
 public class Example {
+
+    @GetMapping("/")
+    public Mono<Test> getTest() {
+        Test test = new Test();
+        test.name = "hello";
+        return Mono.just(test);
+    }
 
     @PostMapping("/")
     public Mono<Test> getTest(@RequestBody final Test test) {
